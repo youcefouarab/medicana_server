d39a81796912c3664c761717d1ef839eb6ec816e
Ouarab <hy_ouarab@esi.dz>
2021-06-15 15:00:14 +0100

    fix: bug in availabilities query

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.get('/auth_doctor/:phone_number', function(req, res, next) {
 });
 
 app.get('/availabilities/:doctor_id/:date/:time', function(req, res, next) {
-    var query = "select * from appointment where doctor_id = ? and date = ? and time >= ? and patient_id = null";
+    var query = "select * from appointment where doctor_id = ? and date = ? and time > ? and patient_id is null";
     connection.query(query, [req.params.doctor_id, req.params.date, req.params.time], function(error, results) {
         if (error) { 
             next(error);
