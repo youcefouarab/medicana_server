749df5127eb5c681e151998b15667b9da8a8fb00
Youcef Ouarab <hy_ouarab@esi.dz>
2021-06-19 19:22:38 +0100

    fix: returned availabilities to date and time

--- index.js
+++ index.js
@@ -102,8 +102,8 @@ app.get('/auth_doctor/:phone_number/:password', function(req, res, next) {
   	});
 });
 
-app.get('/availabilities/:doctor_id/:date_time', function(req, res, next) {
-    var query = "select * from appointment where doctor_id = ? and date_time > ? and patient_id is null";
+app.get('/availabilities/:doctor_id/:date/:time', function(req, res, next) {
+    var query = "select * from appointment where doctor_id = ? and date = ? and time > ? and patient_id is null";
     connection.query(query, [req.params.doctor_id, req.params.date_time], function(error, results) {
         if (error) { 
             next(error);
